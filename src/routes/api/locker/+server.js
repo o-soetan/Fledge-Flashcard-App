1e5aa399879b1f670b2a6b300af027403a931656
Olu Soetan <oes712@gmail.com>
2026-03-20 21:41:09 -0600

    Update +server.js

--- src/routes/api/locker/+server.js
+++ src/routes/api/locker/+server.js
@@ -1,6 +1,5 @@
 import { json } from '@sveltejs/kit';
 import { S3Client, PutObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3';
-import { VITE_R2_ACCOUNT_ID, VITE_R2_ACCESS__KEY_ID, VITE_R2_SECRET_ACCESS_KEY, VITE_R2_BUCKET_NAME } from '$env/static/private';
 import { VITE_R2_ACCOUNT_ID, VITE_R2_ACCESS__KEY_ID, VITE_R2_SECRET_ACCESS_KEY, VITE_R2_BUCKET_NAME } from '$env/static/public';
 
 // Initialize the connection to Cloudflare R2
